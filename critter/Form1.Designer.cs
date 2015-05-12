f54b52f3c58ac72a96dea13b9ad675e2818a3f5e
kmtompkins3@gmail.com <Spencer2>
2015-05-11 21:38:19 -0700

    alot

--- critter/Form1.Designer.cs
+++ critter/Form1.Designer.cs
@@ -28,49 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.progressBar3 = new System.Windows.Forms.ProgressBar();
-            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.HealthBar = new System.Windows.Forms.ProgressBar();
+            this.HungerBar = new System.Windows.Forms.ProgressBar();
+            this.HappienessBar = new System.Windows.Forms.ProgressBar();
+            this.WasteBar = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.WarningBox = new System.Windows.Forms.RichTextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.Feed = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.Clean = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // HealthBar
             // 
-            this.progressBar1.Location = new System.Drawing.Point(525, 35);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(206, 23);
-            this.progressBar1.TabIndex = 0;
+            this.HealthBar.Location = new System.Drawing.Point(525, 35);
+            this.HealthBar.Name = "HealthBar";
+            this.HealthBar.Size = new System.Drawing.Size(206, 23);
+            this.HealthBar.TabIndex = 0;
             // 
-            // progressBar2
+            // HungerBar
             // 
-            this.progressBar2.Location = new System.Drawing.Point(85, 35);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(100, 23);
-            this.progressBar2.TabIndex = 1;
+            this.HungerBar.Location = new System.Drawing.Point(85, 35);
+            this.HungerBar.Name = "HungerBar";
+            this.HungerBar.Size = new System.Drawing.Size(100, 23);
+            this.HungerBar.TabIndex = 1;
             // 
-            // progressBar3
+            // HappienessBar
             // 
-            this.progressBar3.Location = new System.Drawing.Point(85, 64);
-            this.progressBar3.Name = "progressBar3";
-            this.progressBar3.Size = new System.Drawing.Size(100, 23);
-            this.progressBar3.TabIndex = 2;
+            this.HappienessBar.Location = new System.Drawing.Point(85, 64);
+            this.HappienessBar.Name = "HappienessBar";
+            this.HappienessBar.Size = new System.Drawing.Size(100, 23);
+            this.HappienessBar.TabIndex = 2;
             // 
-            // progressBar4
+            // WasteBar
             // 
-            this.progressBar4.Location = new System.Drawing.Point(85, 93);
-            this.progressBar4.Name = "progressBar4";
-            this.progressBar4.Size = new System.Drawing.Size(100, 23);
-            this.progressBar4.TabIndex = 3;
+            this.WasteBar.Location = new System.Drawing.Point(85, 93);
+            this.WasteBar.Name = "WasteBar";
+            this.WasteBar.Size = new System.Drawing.Size(100, 23);
+            this.WasteBar.TabIndex = 3;
             // 
             // label1
             // 
@@ -108,13 +108,13 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Waste:";
             // 
-            // richTextBox1
+            // WarningBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 367);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(618, 62);
-            this.richTextBox1.TabIndex = 8;
-            this.richTextBox1.Text = "";
+            this.WarningBox.Location = new System.Drawing.Point(12, 367);
+            this.WarningBox.Name = "WarningBox";
+            this.WarningBox.Size = new System.Drawing.Size(618, 62);
+            this.WarningBox.TabIndex = 8;
+            this.WarningBox.Text = "";
             // 
             // label5
             // 
@@ -125,61 +125,66 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "Warnings/Errors:";
             // 
-            // button1
+            // Feed
             // 
-            this.button1.Location = new System.Drawing.Point(210, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Feed";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Feed.AutoEllipsis = true;
+            this.Feed.Location = new System.Drawing.Point(210, 35);
+            this.Feed.Name = "Feed";
+            this.Feed.Size = new System.Drawing.Size(75, 23);
+            this.Feed.TabIndex = 10;
+            this.Feed.Text = "Feed";
+            this.Feed.UseVisualStyleBackColor = true;
+            this.Feed.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // Play
             // 
-            this.button2.Location = new System.Drawing.Point(210, 65);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Play";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Play.Location = new System.Drawing.Point(210, 65);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 11;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
             // 
-            // button3
+            // Clean
             // 
-            this.button3.Location = new System.Drawing.Point(210, 93);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Clean";
-            this.button3.UseVisualStyleBackColor = true;
+            this.Clean.Location = new System.Drawing.Point(210, 93);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(75, 23);
+            this.Clean.TabIndex = 12;
+            this.Clean.Text = "Clean";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.Clean_Click);
             // 
-            // button4
+            // Reset
             // 
-            this.button4.Location = new System.Drawing.Point(636, 385);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 23);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Reset";
-            this.button4.UseVisualStyleBackColor = true;
+            this.Reset.Location = new System.Drawing.Point(636, 385);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(95, 23);
+            this.Reset.TabIndex = 13;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(743, 441);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Feed);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.WarningBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar4);
-            this.Controls.Add(this.progressBar3);
-            this.Controls.Add(this.progressBar2);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.WasteBar);
+            this.Controls.Add(this.HappienessBar);
+            this.Controls.Add(this.HungerBar);
+            this.Controls.Add(this.HealthBar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -189,20 +194,20 @@
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ProgressBar progressBar2;
-        private System.Windows.Forms.ProgressBar progressBar3;
-        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar HealthBar;
+        private System.Windows.Forms.ProgressBar HungerBar;
+        private System.Windows.Forms.ProgressBar HappienessBar;
+        private System.Windows.Forms.ProgressBar WasteBar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox WarningBox;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Feed;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Clean;
+        private System.Windows.Forms.Button Reset;
     }
 }
 
--- critter/Form1.cs
+++ critter/Form1.cs
@@ -7,14 +7,295 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace critter
 {
     public partial class Form1 : Form
     {
+        int Health = 100;
+
+        int Hunger = 95;
+        int HungerIncrease = 5;
+        int Happieness = 95;
+        int HappienessIncrease = 5;
+        int Waste = 0;
+
+        //threads
+        Thread HealthIncrease;
+        Thread HungerDecrease;
+        Thread HappienessDecrease;
+        Thread WasteIncrease;
+        //Thread Illness;
+
         public Form1()
         {
             InitializeComponent();
+            HealthBar.Minimum = 0;
+            HealthBar.Maximum = 100;
+            HealthBar.Value = Health;
+            HungerBar.Minimum = 0;
+            HungerBar.Maximum = 150;
+            HungerBar.Value = Hunger;
+            HappienessBar.Minimum = 0;
+            HappienessBar.Maximum = 100;
+            HappienessBar.Value = Happieness;
+            WasteBar.Minimum = 0;
+            WasteBar.Maximum = 100;
+            WasteBar.Value = Waste;
+
+            HealthIncrease = new Thread(new ThreadStart(HealthIncreaseThread));//creates function for thread
+            HealthIncrease.Start();//starts thread
+            HungerDecrease = new Thread(new ThreadStart(HungerDecreaseThread));//creates function for thread
+            HungerDecrease.Start();//starts thread
+            HappienessDecrease = new Thread(new ThreadStart(HappienessDecreaseThread));//creates function for thread
+            HappienessDecrease.Start();//starts thread
+            WasteIncrease = new Thread(new ThreadStart(WasteIncreaseThread));//creates function for thread
+            WasteIncrease.Start();//starts thread
+
         }
+
+        #region FeedButton
+        //feed button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hunger = Hunger + HungerIncrease;
+            HungerBar.Value = Hunger;
+            if (Hunger <= 100 && Happieness <= 95)
+            {
+                HungerIncrease = 5;
+                Happieness = Happieness + 5;
+                HappienessBar.Value = Happieness;
+                Waste = Waste + 5;
+                WasteBar.Value = Waste;
+
+                if (Health <= 95)
+                {
+                    Health = Health + 5;
+                    HealthBar.Value = Health;
+                }
+            }
+            if (Hunger >= 110 && Waste < 90)
+            {
+                Happieness = Happieness - 5;
+                HappienessBar.Value = Happieness;
+                Waste = Waste + 10;
+                WasteBar.Value = Waste;
+            }
+            //here
+            if (Hunger >= 100)
+            {
+                WarningBox.Text = "You are going to over feed your critter";
+            }
+            if (Hunger >= 150)
+            {
+                WarningBox.Text = "You have over fed your critter there will be consequences";
+                HungerIncrease = 0;
+                if (Health >= 5)
+                {
+                    Health = Health - 5;
+                    HealthBar.Value = Health;
+                }
+                if (Health <= 0)
+                {
+                    WarningBox.Text = "Your critter is dead";
+                }
+            }
+        }
+        #endregion
+
+        #region PlayButton
+        private void Play_Click(object sender, EventArgs e)
+        {
+            Happieness = Happieness + HappienessIncrease;
+            if (Happieness >= 95 && Health != 0)
+            {
+                WarningBox.Text = "Your going to over work your critter";
+                if (Hunger >= 10)
+                {
+                    Hunger = Hunger - 10;
+                    HungerBar.Value = Hunger;
+                }
+                if (Health >= 5)
+                {
+                    Health = Health - 5;
+                    HealthBar.Value = Health;
+                    if (Health <= 0)
+                    {
+                        WarningBox.Text = "Your critter is dead";
+                    }
+                }
+                if (Happieness > 5)
+                {
+                    Happieness = Happieness - 40;
+                    HappienessBar.Value = Happieness;
+                }
+            }
+            if (Happieness < 95)
+            {
+                Happieness = Happieness + 5;
+                HappienessBar.Value = Happieness;
+            }
+            if (Happieness == 0)
+            {
+                Health = Health - 10;
+                HealthBar.Value = Health;
+            }
+        }
+        #endregion
+
+        #region CleanButton
+
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            if (Waste != 0)
+            {
+                Waste = Waste - 5;
+                WasteBar.Value = Waste;
+                if (Happieness <= 95)
+                {
+                    Happieness = Happieness + 5;
+                    HappienessBar.Value = Happieness;
+                }
+            }
+        }
+
+        #endregion
+
+        #region ResetButton
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Health = 100;
+            Hunger = 95;
+            HungerIncrease = 5;
+            Happieness = 95;
+            HappienessIncrease = 5;
+            Waste = 0;
+            HealthBar.Minimum = 0;
+            HealthBar.Maximum = 100;
+            HealthBar.Value = Health;
+            HungerBar.Minimum = 0;
+            HungerBar.Maximum = 150;
+            HungerBar.Value = Hunger;
+            HappienessBar.Minimum = 0;
+            HappienessBar.Maximum = 100;
+            HappienessBar.Value = Happieness;
+            WasteBar.Minimum = 0;
+            WasteBar.Maximum = 100;
+            WasteBar.Value = Waste;
+
+            WarningBox.Text = "Critter Reset";
+        }
+
+        #endregion
+
+        private void HealthIncreaseThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    Random rnd = new Random();
+                    int RandomHealth = rnd.Next(1, 6);
+
+                    if (Health <= 95 && Health != 0)
+                    {
+                        switch (RandomHealth)
+                        {
+                            case 1:
+                                Health = Health + 0;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 2:
+                                Health = Health + 1;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 3:
+                                Health = Health + 2;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 4:
+                                Health = Health + 3;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 5:
+                                Health = Health + 3;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 6:
+                                Health = Health + 4;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                        }
+                    }
+                    Thread.Sleep(5000);
+                }
+
+            }
+            catch (ThreadAbortException tbe)
+            {
+                HealthIncrease.Abort();//ends thread
+            }
+        }
+
+        private void WasteIncreaseThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    Random rnd = new Random();
+                    int RandomWaste= rnd.Next(1, 6);
+
+                    if (Waste <= 95 && Waste != 0)
+                    {
+                        switch (RandomWaste)
+                        {
+                            case 1:
+                                Waste = Waste + 0;
+                                WasteBar.Invoke((MethodInvoker)(() => WasteBar.Value = Waste));
+                                break;
+                            case 2:
+                                Waste = Waste + 1;
+                                WasteBar.Invoke((MethodInvoker)(() => WasteBar.Value = Waste));
+                                break;
+                            case 3:
+                                Waste = Health + 2;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 4:
+                                Health = Health + 3;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 5:
+                                Health = Health + 3;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                            case 6:
+                                Health = Health + 4;
+                                HealthBar.Invoke((MethodInvoker)(() => HealthBar.Value = Health));
+                                break;
+                        }
+                    }
+                    Thread.Sleep(5000);
+                }
+
+            }
+            catch (ThreadAbortException tbe)
+            {
+                HealthIncrease.Abort();//ends thread
+            }
+        }
+
+        private void HappienessDecreaseThread()
+        {
+
+        }
+
+        private void HungerDecreaseThread()
+        {
+
+        }
+
     }
 }
